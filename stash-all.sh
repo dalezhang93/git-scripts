c1707d263171a6eac874e811b89dab228741d7b9
zhangduo <zhangduo914@gmail.com>
2019-05-24 13:58:48 +0800

    :sparkles: stash 分为只保存工作区内容和所有内容两个脚本

---
+++ stash-all.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git stash push -all temp-`date +%s`
+
--- stash-workspace.sh
+++ stash-workspace.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-# 保留缓存区
-# 清空未追踪文件
-# stash 名字随机
-
-git stash save --keep-index --include-untracked temp-`date +%s`
+git stash push --keep-index --include-untracked temp-`date +%s`
 
